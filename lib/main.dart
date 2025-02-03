a5d284403f4f30d09eeeffed14306505708378d2
TehminaFatima <tehmina.sgr@gmail.com>
2025-02-03 22:50:06 +0500

    packages added

--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,22 @@ import 'package:supabase_authentiation/auth_service/auth_gate.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:get/get.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Ensures Flutter is initialized
 
+  // Initialize Supabase
   await Supabase.initialize(
     url: 'https://gdhhujsvqjcgbuqdcrvu.supabase.co',
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkaGh1anN2cWpjZ2J1cWRjcnZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5NjQyOTIsImV4cCI6MjA1MjU0MDI5Mn0.DynyQasCUAxXA6i_SVK8DyuOV4zb8ynWBKEfIQF0T8w',
   );
+
+  // Initialize Hive
   await Hive.initFlutter();
-  await Hive.openBox<String>(
-      'textList'); // Open Hive box before running the app
-  await Hive.openBox<bool>('checkList'); // Open for checkbox states
+  await Future.wait([
+    Hive.openBox<String>('textList'),
+    Hive.openBox<bool>('checkList'),
+  ]);
 
   runApp(const MyApp());
 }
@@ -24,7 +28,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
